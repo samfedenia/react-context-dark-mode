1c99066e6cacc6ddefc0ca782e13b53ee6e46f1d
samfedenia <sam@fedenia.xyz>
2022-04-14 10:40:39 -0500

    add context dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { useTheme, useThemeUpdate } from './ThemeContext';
+import { Sun, Moon, IconAttribution } from './Icons';
 function App() {
-  return <div></div>;
+  const darkTheme = useTheme();
+  const toggleTheme = useThemeUpdate();
+  const themeStyles = {
+    backgroundColor: darkTheme ? '#333' : '#EEE',
+    color: darkTheme ? '#999' : '#111',
+    padding: '3rem',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContext: 'center',
+  };
+
+  const linkStyles = {
+    color: darkTheme ? 'pink' : 'darkgray',
+    textDecoration: 'none',
+    paddingTop: '2rem',
+  };
+  return (
+    <div style={themeStyles}>
+      <div
+        style={{
+          display: 'flex',
+          width: '80vw',
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+          justifyContent: 'flex-end',
+        }}
+      >
+        {darkTheme ? <Sun onClick={toggleTheme} /> : <Moon onClick={toggleTheme} />}
+      </div>
+      <h2>Dark mode toggle using React Context</h2>
+      <IconAttribution style={linkStyles} />
+    </div>
+  );
 }
 
 export default App;
